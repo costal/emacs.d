8f97b7147dfe4f911e4d8c039ff149bd210f34ce
Leonel Da Costa <leodacosta93@gmail.com>
2026-03-16 03:50:20 -0400

    Enhance Flymake configuration for improved functionality and usability
    - Updated Flymake setup to use built-in support since Emacs 26.
    - Refined integration with Flycheck by disabling overlapping checkers.
    - Streamlined hook definitions using the shorthand `#'` syntax.
    - Improved eldoc integration for Emacs versions 28.1 and above.
    - Added Flycheck-like keybindings for better user experience in Flymake mode.

--- lisp/init-flymake.el
+++ lisp/init-flymake.el
@@ -1,35 +1,44 @@
-;;; init-flymake.el --- Configure Flymake global behaviour -*- lexical-binding: t -*-
+;;; init-flymake.el --- Configure Flymake behaviour -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
 
-(maybe-require-package 'flymake "1.2.1")
+;; Flymake is built-in since Emacs 26
+(require 'flymake)
 
-;; Use flycheck checkers with flymake, to extend its coverage
+;; Optional: use Flycheck checkers through Flymake
 (when (maybe-require-package 'flymake-flycheck)
-  ;; Disable flycheck checkers for which we have flymake equivalents
-  (with-eval-after-load 'flycheck
-    (setq-default
-     flycheck-disabled-checkers
-     (append (default-value 'flycheck-disabled-checkers)
-             '(emacs-lisp emacs-lisp-checkdoc emacs-lisp-package sh-shellcheck))))
 
-  (add-hook 'flymake-mode-hook 'flymake-flycheck-auto)
-  (add-hook 'prog-mode-hook 'flymake-mode)
-  (add-hook 'text-mode-hook 'flymake-mode))
+  ;; Disable overlapping Flycheck checkers
+  (with-eval-after-load 'flycheck
+    (setq flycheck-disabled-checkers
+          (append flycheck-disabled-checkers
+                  '(emacs-lisp
+                    emacs-lisp-checkdoc
+                    emacs-lisp-package
+                    sh-shellcheck))))
+
+  (add-hook 'flymake-mode-hook #'flymake-flycheck-auto))
+
+;; Enable Flymake automatically
+(add-hook 'prog-mode-hook #'flymake-mode)
+(add-hook 'text-mode-hook #'flymake-mode)
 
 (with-eval-after-load 'flymake
-  ;; Provide some flycheck-like bindings in flymake mode to ease transition
-  (define-key flymake-mode-map (kbd "C-c ! l") 'flymake-show-buffer-diagnostics)
-  (define-key flymake-mode-map (kbd "C-c ! n") 'flymake-goto-next-error)
-  (define-key flymake-mode-map (kbd "C-c ! p") 'flymake-goto-prev-error)
-  (define-key flymake-mode-map (kbd "C-c ! c") 'flymake-start))
+  ;; Flycheck-like keybindings
+  (define-key flymake-mode-map (kbd "C-c ! l") #'flymake-show-buffer-diagnostics)
+  (define-key flymake-mode-map (kbd "C-c ! n") #'flymake-goto-next-error)
+  (define-key flymake-mode-map (kbd "C-c ! p") #'flymake-goto-prev-error)
+  (define-key flymake-mode-map (kbd "C-c ! c") #'flymake-start))
 
-(unless (version< emacs-version "28.1")
-  (setq eldoc-documentation-function 'eldoc-documentation-compose)
+;; Better eldoc integration (Emacs ≥28)
+(when (version<= "28.1" emacs-version)
+  (setq eldoc-documentation-strategy #'eldoc-documentation-compose)
 
   (add-hook 'flymake-mode-hook
             (lambda ()
-              (add-hook 'eldoc-documentation-functions 'flymake-eldoc-function nil t))))
+              (add-hook 'eldoc-documentation-functions
+                        #'flymake-eldoc-function
+                        nil t))))
 
 (provide 'init-flymake)
 ;;; init-flymake.el ends here
