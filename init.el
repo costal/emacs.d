294873474c2edd8c5da1c759835d3002414cb71a
Leonel Da Costa <leodacosta93@gmail.com>
2026-03-16 03:12:36 -0400

    Refactor init.el for improved configuration management
    - Adjusted garbage collection thresholds for better startup performance.
    - Enabled spell check support by default.
    - Updated installation mode handling and added error checking for invalid modes.
    - Cleaned up comments and organized require statements for clarity.
    - Replaced deprecated package requires and added new packages for enhanced functionality.

--- init.el
+++ init.el
@@ -1,13 +1,8 @@
-;;; init.el --- Load the full configuration -*- lexical-binding: t -*-
-;;; Commentary:
+;; -*- lexical-binding: t -*-
+(setq debug-on-error t)
 
-;; This file bootstraps the configuration, which is divided into
-;; a number of other files.
-
-;;; Code:
-
-;; Produce backtraces when errors occur: can be helpful to diagnose startup issues
-;;(setq debug-on-error t)
+;;; This file bootstraps the configuration, which is divided into
+;;; a number of other files.
 
 (let ((minver "27.1"))
   (when (version< emacs-version minver)
@@ -18,48 +13,57 @@
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (require 'init-benchmarking) ;; Measure startup time
 
-(defconst *spell-check-support-enabled* nil) ;; Enable with t if you prefer
+(defconst *spell-check-support-enabled* t) ;; Enable with t if you prefer
 (defconst *is-a-mac* (eq system-type 'darwin))
+(defconst *is-a-linux* (eq system-type 'gnu/linux))
+(defconst *is-a-windows* (eq system-type 'windows-nt))
 
-
-;; Adjust garbage collection threshold for early startup (see use of gcmh below)
-(setq gc-cons-threshold (* 128 1024 1024))
+(setq ad-redefinition-action 'accept) ;; ignore the redefinition warning
+(setq debug-on-error nil) ;; turn off debug-on-error
 
-
-;; Process performance tuning
+;;----------------------------------------------------------------------------
+;; Set installation mode: text, programming, full
+;;----------------------------------------------------------------------------
+(setq install-mode "full")
 
-(setq read-process-output-max (* 4 1024 1024))
-(setq process-adaptive-read-buffering nil)
+(let ((install-modes '("full" "text" "programming")))
+  (when (not (member install-mode install-modes))
+	(error "Set install-mode from full, text or programming")))
 
-
+;;----------------------------------------------------------------------------
+;; Adjust garbage collection thresholds during startup, and thereafter
+;;----------------------------------------------------------------------------
+(let ((normal-gc-cons-threshold (* 20 1024 1024))
+      (init-gc-cons-threshold (* 128 1024 1024)))
+  (setq gc-cons-threshold init-gc-cons-threshold)
+  (add-hook 'after-init-hook
+            (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
+
+;;----------------------------------------------------------------------------
 ;; Bootstrap config
-
-
-(setq custom-file (locate-user-emacs-file "custom.el"))
+;;----------------------------------------------------------------------------
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (require 'init-utils)
 (require 'init-site-lisp) ;; Must come before elpa, as it may provide package.el
 ;; Calls (package-initialize)
 (require 'init-elpa)      ;; Machinery for installing required packages
 (require 'init-exec-path) ;; Set up $PATH
 
-
-;; General performance tuning
-(when (require-package 'gcmh)
-  (setq gcmh-high-cons-threshold (* 128 1024 1024))
-  (add-hook 'after-init-hook (lambda ()
-                               (gcmh-mode)
-                               (diminish 'gcmh-mode))))
-
-(setq jit-lock-defer-time 0)
-
-
+;;----------------------------------------------------------------------------
 ;; Allow users to provide an optional "init-preload-local.el"
+;;----------------------------------------------------------------------------
 (require 'init-preload-local nil t)
 
+;;----------------------------------------------------------------------------
 ;; Load configs for specific features and modes
+;;----------------------------------------------------------------------------
+
+(require-package 'wgrep)
 (require-package 'diminish)
-(maybe-require-package 'scratch)
+(require-package 'scratch)
 (require-package 'command-log-mode)
+
+(require 'init-package-tracking)
 
 (require 'init-frame-hooks)
 (require 'init-xterm)
@@ -67,23 +71,25 @@
 (require 'init-osx-keys)
 (require 'init-gui-frames)
 (require 'init-dired)
+(require 'init-neotree)
 (require 'init-isearch)
 (require 'init-grep)
 (require 'init-uniquify)
 (require 'init-ibuffer)
-(require 'init-flymake)
-(require 'init-eglot)
+(require 'init-flycheck)
 
 (require 'init-recentf)
-(require 'init-minibuffer)
+(require 'init-smex)
+(require 'init-ivy)
 (require 'init-hippie-expand)
-(require 'init-corfu)
+(require 'init-company)
 (require 'init-windows)
 (require 'init-sessions)
-(require 'init-mmm)
+(require 'init-fonts)
 
 (require 'init-editing-utils)
 (require 'init-whitespace)
+(require 'init-fci)
 
 (require 'init-vc)
 (require 'init-darcs)
@@ -93,48 +99,33 @@
 (require 'init-projectile)
 
 (require 'init-compile)
-(require 'init-crontab)
-(require 'init-textile)
-(require 'init-markdown)
 (require 'init-csv)
-(require 'init-erlang)
-(require 'init-javascript)
-(require 'init-php)
 (require 'init-org)
-(require 'init-nxml)
-(require 'init-html)
-(require 'init-css)
-(require 'init-haml)
-(require 'init-http)
-(require 'init-python)
-(require 'init-haskell)
-(require 'init-elm)
-(require 'init-purescript)
-(require 'init-ruby)
-(require 'init-rails)
-(require 'init-sql)
-(require 'init-ocaml)
-(require 'init-j)
-(require 'init-nim)
-(require 'init-rust)
-(require 'init-toml)
-(require 'init-yaml)
-(require 'init-docker)
-(require 'init-terraform)
-(require 'init-nix)
-(maybe-require-package 'nginx-mode)
-(maybe-require-package 'just-mode)
-(when (maybe-require-package 'just-ts-mode)
-  ;; Undo overly-optimistic autoloading, so that things still work in
-  ;; Emacs 29 without treesitter
-  (sanityinc/remove-auto-mode  'just-ts-mode))
-(maybe-require-package 'justl)
+(require 'init-deft)
+(require 'init-code)
 
+(require 'init-yasnippet)
+
+(when (not (string-equal install-mode "text"))
+  (require 'init-nxml)
+  (require 'init-html)
+  (require 'init-python-mode)
+  (require 'init-cc-mode)
+  (when *is-a-linux*
+;;    (require 'init-ros))
+;;  (require 'init-csharp)
+;;  (require 'init-matlab-mode)
+;;  (require 'init-arduino))
+
+(when (not *is-a-windows*)
+  (require 'init-multi-term))
+
+(require 'init-yaml)
 (require 'init-paredit)
 (require 'init-lisp)
-(require 'init-sly)
-(require 'init-clojure)
-(require 'init-clojure-cider)
+(require 'init-slime)
+
+(require 'init-common-lisp)
 
 (when *spell-check-support-enabled*
   (require 'init-spelling))
@@ -143,60 +134,61 @@
 
 (require 'init-folding)
 (require 'init-dash)
-
 (require 'init-ledger)
-(require 'init-lua)
-(require 'init-uiua)
-(require 'init-zig)
-(require 'init-terminals)
 
+(when (not (string-equal install-mode "programming"))
+  (require 'init-markdown)
+  (require 'init-tex)
+  ;; (require 'init-ac-math)
+  (require 'init-company-math)
+;;  (require 'init-mail)
+  (require 'init-blog))
+
+(require 'init-ssh)
 ;; Extra packages which don't require any configuration
-
-(require-package 'sudo-edit)
-(maybe-require-package 'gnuplot)
+(require-package 'gnuplot)
 (require-package 'htmlize)
+;; (when (not *is-a-windows*)
+;;   (require-package 'dsvn)
+;;   (autoload 'svn-status "dsvn" "Run `svn status'." t)
+;;   (autoload 'svn-update "dsvn" "Run `svn update'." t)
+;;   )
+;; (require-package 'psvn)
 (when *is-a-mac*
   (require-package 'osx-location))
+(maybe-require-package 'regex-tool)
 (maybe-require-package 'dotenv-mode)
-(maybe-require-package 'shfmt)
 
 (when (maybe-require-package 'uptimes)
   (setq-default uptimes-keep-count 200)
   (add-hook 'after-init-hook (lambda () (require 'uptimes))))
 
-(when (fboundp 'global-eldoc-mode)
-  (add-hook 'after-init-hook 'global-eldoc-mode))
 
-(require 'init-direnv)
-
-(when (and (require 'treesit nil t)
-           (fboundp 'treesit-available-p)
-           (treesit-available-p))
-  (require 'init-treesitter))
-
-
-
+;;----------------------------------------------------------------------------
 ;; Allow access from emacsclient
-(add-hook 'after-init-hook
-          (lambda ()
-            (require 'server)
-            (unless (server-running-p)
-              (server-start))))
+;;----------------------------------------------------------------------------
+(require 'server)
+(unless (server-running-p)
+  (server-start))
 
+;;----------------------------------------------------------------------------
 ;; Variables configured via the interactive 'customize' interface
+;;----------------------------------------------------------------------------
 (when (file-exists-p custom-file)
   (load custom-file))
 
+
+;;----------------------------------------------------------------------------
 ;; Locales (setting them earlier in this file doesn't work in X)
+;;----------------------------------------------------------------------------
 (require 'init-locales)
 
+
+;;----------------------------------------------------------------------------
 ;; Allow users to provide an optional "init-local" containing personal settings
+;;----------------------------------------------------------------------------
 (require 'init-local nil t)
 
-(provide 'init)
 
-;; Local Variables:
-;; coding: utf-8
-;; no-byte-compile: t
-;; End:
-;;; init.el ends here
+
+(provide 'init)
